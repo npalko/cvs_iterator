158a360856278ed2aca58de6c00d7ac3da938047
Nicholas Palko <nicholas.palko@gmail.com>
2013-01-01 19:17:50 -0500

    init

---
+++ include/csv_iterator.hpp
@@ -0,0 +1,91 @@
+//                     Copyright Nicholas Palko 2012
+//      Distributed under the Boost Software License, Version 1.0.
+//          (See accompanying file LICENSE_1_0.txt or copy at
+//                 http://www.boost.org/LICENSE_1_0.txt)
+
+// http://tools.ietf.org/html/rfc4180
+
+#ifndef CSV_ITERATOR_HPP
+#define CSV_ITERATOR_HPP
+
+#include <iterator>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+namespace csv {
+
+template<class T, class Factory>
+class iterator : public std::iterator<
+          std::input_iterator_tag,
+          T,
+          std::size_t,
+          T*,
+          T&
+          > {
+ private:
+  std::istream* is_;
+  Factory factory_;
+  char delim_;
+  int skip_;
+  int skipped_;
+  T current_;
+ public:
+  iterator () : is_(nullptr) {}
+  iterator (std::istream& is, Factory factory, char delim = ',', int skip = 0)
+      : is_(&is),
+        factory_(factory),
+        delim_(delim),
+        skip_(skip),
+        skipped_(0) {
+    ++(*this);
+  }
+  const T& operator* () const {
+    return current_;
+  }
+  const T* operator-> () const {
+    return current_;
+  }
+  bool operator== (iterator const& rhs) {
+    return (is_ == nullptr) && (rhs.is_ == nullptr);
+  }
+  bool operator!= (iterator const& rhs) {
+    return !((*this) == rhs);
+  }
+  iterator& operator++ () {
+    if (is_ == nullptr) {
+      return *this;
+    }
+    if (!is_->good()) {
+      is_ = nullptr;
+      return *this;
+    }
+    
+    std::string line, field;
+    std::vector<std::string> fields;
+    
+    while (skipped_ < skip_) {    // skip over file header 
+      std::getline(*is_, line);
+      ++skipped_;
+    }
+    
+    std::getline (*is_, line);
+    std::stringstream linestream(line);
+    while (std::getline (linestream, field, delim_)) {
+      fields.push_back (field);
+    }
+    
+    if (fields.empty()) {         // in the case of an empty file
+      is_ = nullptr;
+      return *this;
+    }
+    
+    current_ = factory_ (fields);
+    return *this;
+  }
+};
+
+};
+
+#endif
---
+++ test/test.cpp
@@ -0,0 +1,12 @@
+
+
+#include "gtest/gtest.h"
+
+#include "csv_iterator.hpp"
+
+
+TEST(File, EmptyFile) {
+
+
+}
+
